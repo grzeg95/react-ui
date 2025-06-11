45da810dbb5299707a9a9ec1bd8fcf03e70ecfad
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-06-11 14:09:36 +0200

    feat: TextField

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import './App.scss';
+import {TextField} from './components/TextField/TextField';
 
 function App() {
+
   return (
-    <div>App</div>
-  )
+    <>
+      <TextField
+        placeholder="Placeholder"
+        label="Label"
+      />
+      <p></p>
+      <TextField
+        placeholder="Placeholder"
+        label="Label"
+        error="Error text"
+      />
+      <p></p>
+      <TextField
+        placeholder="Placeholder"
+        label="Disabled"
+        disabled={true}
+      />
+    </>
+  );
 }
 
 export default App
---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,79 @@
+import './TextField.scss';
+import {type ChangeEventHandler, type RefObject, useRef, useState} from 'react';
+
+interface TextFieldProps {
+  type?: 'text' | 'number';
+  required?: boolean;
+  placeholder?: string;
+  label?: string;
+  id?: string;
+  disabled?: boolean;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  ref?: RefObject<HTMLInputElement | null>;
+  error?: string;
+}
+
+let _id = 1;
+
+export function TextField(
+  {
+    type = 'text',
+    required = false,
+    placeholder,
+    label,
+    id,
+    disabled,
+    onChange = () => {},
+    ref,
+    error
+  }: TextFieldProps
+) {
+
+  const _inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = ref || _inputRef;
+
+  const [focused, setFocused] = useState(false);
+  const [inputId] = useState<string>(id ? id : 'TextField_' + _id++);
+
+  const labelFloating = inputRef.current?.value || focused;
+
+  return (
+    <div className="text-field-wrapper">
+      <div
+        className={`text-field ${focused ? 'text-field--focused' : ''} ${error ? 'text-field--error' : ''} ${disabled ? 'text-field--disabled' : ''}`}
+      >
+        <label
+          className={`text-field-label ${labelFloating ? "text-field-label--floating" : ""}`}
+          htmlFor={inputId}
+        >{label}</label>
+
+        <input
+          ref={inputRef}
+          className={`text-field-input ${!labelFloating ? "text-field-input--hide-placeholder" : ""}`}
+          required={required}
+          type={type}
+          id={inputId}
+          placeholder={placeholder}
+          onBlur={() => {
+            setFocused(false);
+          }}
+          onFocus={() => {
+            setFocused(true);
+          }}
+          onChange={onChange}
+        />
+
+        <div
+          className={`text-field-underline ${focused ? 'text-field-underline--active' : ''}`}
+        ></div>
+      </div>
+
+      <div className="text-field-error-wrapper">
+        <div className="text-field-error">
+          {error}
+        </div>
+      </div>
+    </div>
+
+  );
+}
